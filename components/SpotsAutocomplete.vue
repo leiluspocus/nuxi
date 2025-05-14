c1ef953891538ab0e015d5e30e387d5c9984daaf
* Laila Atrmouh <leiluspocus@users.noreply.github.com>
2025-05-14 16:55:58 +0200

    style: make suggestions more visible

--- components/SpotsAutocomplete.vue
+++ components/SpotsAutocomplete.vue
@@ -29,7 +29,6 @@ const fetchSuggestions = async (e) => {
     for (const item of features) {
       suggestions.value = [...suggestions.value, item.properties]
     }
-    console.log(suggestions.value)
     autoloaderInProgress.value = false
   }
 }
@@ -43,7 +42,7 @@ const fetchSuggestions = async (e) => {
     placeholder="Tapez le nom et/ou l'adresse ..."
     v-model="newProposal.formatted"
     @input="fetchSuggestions"
-    class="autocomplete-container block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
+    class="autocomplete-container block w-full p-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
   />
   <div v-show="suggestions.length > 0" class="autocomplete-items">
     <div
@@ -51,7 +50,7 @@ const fetchSuggestions = async (e) => {
       v-for="(item, index) in suggestions"
       :data-lat="item.lat"
       :data-lon="item.lon"
-      class="dark:text-black"
+      class="dark:text-black m-2"
       @click="
         () => {
           suggestions = []
@@ -71,7 +70,10 @@ const fetchSuggestions = async (e) => {
 .autocomplete-items {
   position: absolute;
   border: 1px solid rgba(0, 0, 0, 0.1);
-  box-shadow: 0px 2px 10px 2px rgba(0, 0, 0, 0.1);
+  box-shadow: 0px 2px 10px 2px rgba(0, 0, 0, 0.3);
+  margin-left: 18px;
+  margin-right: 18px;
+  margin-top: 8px;
   border-top: none;
   background-color: #fff;
 
